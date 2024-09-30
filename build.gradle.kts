ab2e924a3bb4d5f4464a9bb241b49b32466ef08b
Igor Gunin <gunin-igor75@yandex.ru>
2024-09-30 19:17:03 +0600

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+
+    testImplementation("app.cash.turbine:turbine:1.1.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.24")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation ("org.junit.jupiter:junit-jupiter-params:5.1.0")
 }
 
 tasks.test {
